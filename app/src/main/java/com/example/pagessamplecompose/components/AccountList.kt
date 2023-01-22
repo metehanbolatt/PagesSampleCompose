dc265437318d9a4afe054368d4d20d5e174a3ab8
Metehan <metehan.bolat.ie@gmail.com>
2023-01-22 12:49:24 +0300

    Account Chooser Page was completed.

---
+++ app/src/main/java/com/example/pagessamplecompose/components/AccountList.kt
@@ -0,0 +1,29 @@
+package com.example.pagessamplecompose.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pagessamplecompose.data.AccountModel
+import com.example.pagessamplecompose.ui.theme.ExtraLightGray
+
+@Composable
+fun AccountList(
+    accountList: List<AccountModel>,
+    onAccountClicked: (AccountModel) -> Unit
+) {
+    val listState = rememberLazyListState()
+    LazyColumn(
+        state = listState,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(ExtraLightGray)
+    ) {
+        items(accountList) { accountModel ->
+            AccountCardItem(accountModel = accountModel, onAccountClicked = onAccountClicked)
+        }
+    }
+}
