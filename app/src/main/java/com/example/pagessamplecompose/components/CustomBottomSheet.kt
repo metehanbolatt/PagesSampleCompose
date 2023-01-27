41e709709d53be732f3283abf484bc11a9d4c0f0
Metehan <metehan.bolat.ie@gmail.com>
2023-01-28 00:45:36 +0300

    CustomBottomSheet was completed.

--- app/src/main/java/com/example/pagessamplecompose/components/CustomBottomSheet.kt
+++ app/src/main/java/com/example/pagessamplecompose/components/CustomBottomSheet.kt
@@ -1,64 +1,22 @@
 package com.example.pagessamplecompose.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.Text
-import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pagessamplecompose.data.accountList
-import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @Composable
-fun CustomBottomSheet() {
-    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
-    val scope = rememberCoroutineScope()
+fun CustomBottomSheet(
+    sheetState: ModalBottomSheetState,
+    content: @Composable () -> Unit,
+    sheetContent: @Composable () -> Unit
+) {
     ModalBottomSheetLayout(
         sheetState = sheetState,
-        sheetContent = {
-            AccountList(
-                accountList = accountList,
-                onAccountClicked = {
-                    println("Account Model: $it")
-                }
-            )
-        },
+        sheetContent = { sheetContent() },
         sheetShape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp)
-    ) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Button(
-                onClick = {
-                    scope.launch {
-                        if (sheetState.isVisible) {
-                            sheetState.hide()
-                        } else {
-                            sheetState.show()
-                        }
-                    }
-                }
-            ) {
-                Text(text = "Toggle Sheet")
-            }
-        }
-    }
-}
-
-@ExperimentalMaterialApi
-@Preview
-@Composable
-fun CustomBottomSheetPreview() {
-    CustomBottomSheet()
+    ) { content() }
 }
--- app/src/main/java/com/example/pagessamplecompose/pages/UserInformationPage.kt
+++ app/src/main/java/com/example/pagessamplecompose/pages/UserInformationPage.kt
@@ -17,8 +17,13 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.pagessamplecompose.R
+import com.example.pagessamplecompose.components.AccountList
+import com.example.pagessamplecompose.components.CustomBottomSheet
+import com.example.pagessamplecompose.data.accountList
 import com.example.pagessamplecompose.ui.theme.BoldGreen
+import kotlinx.coroutines.launch
 
+@ExperimentalMaterialApi
 @Composable
 fun UserInformationPage(
     navController: NavHostController,
@@ -31,6 +36,8 @@ fun UserInformationPage(
     var amount by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
 
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val scope = rememberCoroutineScope()
     val scrollState = rememberScrollState()
 
     Scaffold(
@@ -61,208 +68,233 @@ fun UserInformationPage(
             )
         },
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it)
-                    .verticalScroll(scrollState)
-            ) {
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp, top = 20.dp, end = 20.dp),
-                    value = recipient,
-                    onValueChange = { recipient = it },
-                    label = {
-                        Text(text = "Alıcının Adı Soyadı")
-                    },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = BoldGreen,
-                        focusedLabelColor = BoldGreen,
-                        cursorColor = BoldGreen
-                    )
-                )
-
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, top = 20.dp),
-                    value = iban,
-                    onValueChange = { iban = it },
-                    label = {
-                        Text(text = "IBAN")
-                    },
-                    trailingIcon = {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_camera),
-                            contentDescription = null,
-                            tint = BoldGreen
+            CustomBottomSheet(
+                sheetState = sheetState,
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(it)
+                            .verticalScroll(scrollState)
+                    ) {
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, top = 20.dp, end = 20.dp),
+                            value = recipient,
+                            onValueChange = { recipient = it },
+                            label = {
+                                Text(text = "Alıcının Adı Soyadı")
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = BoldGreen,
+                                focusedLabelColor = BoldGreen,
+                                cursorColor = BoldGreen
+                            )
                         )
-                    },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = BoldGreen,
-                        focusedLabelColor = BoldGreen,
-                        cursorColor = BoldGreen
-                    )
-                )
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 5.dp, end = 20.dp, top = 20.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Checkbox(
-                        checked = saveRecipient,
-                        onCheckedChange = { saveRecipient = it },
-                        colors = CheckboxDefaults.colors(
-                            uncheckedColor = BoldGreen,
-                            checkedColor = BoldGreen
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, end = 20.dp, top = 20.dp),
+                            value = iban,
+                            onValueChange = { iban = it },
+                            label = {
+                                Text(text = "IBAN")
+                            },
+                            trailingIcon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_camera),
+                                    contentDescription = null,
+                                    tint = BoldGreen
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = BoldGreen,
+                                focusedLabelColor = BoldGreen,
+                                cursorColor = BoldGreen
+                            )
                         )
-                    )
-                    Text(text = "Alıcıyı Kaydet")
-                }
 
-                Spacer(modifier = Modifier.height(10.dp))
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 5.dp, end = 20.dp, top = 20.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Checkbox(
+                                checked = saveRecipient,
+                                onCheckedChange = { saveRecipient = it },
+                                colors = CheckboxDefaults.colors(
+                                    uncheckedColor = BoldGreen,
+                                    checkedColor = BoldGreen
+                                )
+                            )
+                            Text(text = "Alıcıyı Kaydet")
+                        }
 
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "İŞLEM BİLGİLERİ",
-                    textAlign = TextAlign.Center
-                )
+                        Spacer(modifier = Modifier.height(10.dp))
 
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, top = 20.dp),
-                    value = senderAccount,
-                    onValueChange = { senderAccount = it },
-                    label = {
-                        Text(text = "Gönderen Hesap")
-                    },
-                    trailingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowDown,
-                            contentDescription = null,
-                            tint = BoldGreen
-                        )
-                    },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = BoldGreen,
-                        focusedLabelColor = BoldGreen,
-                        cursorColor = BoldGreen
-                    )
-                )
-
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, top = 20.dp),
-                    value = amount,
-                    onValueChange = { amount = it },
-                    label = {
-                        Text(text = "Gönderilecek Tutar")
-                    },
-                    trailingIcon = {
                         Text(
-                            text = "Tüm Bakiye",
-                            modifier = Modifier.padding(end = 10.dp),
-                            color = BoldGreen
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "İŞLEM BİLGİLERİ",
+                            textAlign = TextAlign.Center
                         )
-                    },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = BoldGreen,
-                        focusedLabelColor = BoldGreen,
-                        cursorColor = BoldGreen
-                    )
-                )
 
-                Row {
-                    OutlinedTextField(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .padding(start = 20.dp, end = 10.dp, top = 20.dp),
-                        value = "",
-                        onValueChange = { },
-                        label = {
-                            Text(text = "Bugün")
-                        },
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowDown,
-                                contentDescription = null,
-                                tint = BoldGreen
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, end = 20.dp, top = 20.dp),
+                            value = senderAccount,
+                            onValueChange = { senderAccount = it },
+                            label = {
+                                Text(text = "Gönderen Hesap")
+                            },
+                            trailingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowDown,
+                                    contentDescription = null,
+                                    tint = BoldGreen
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = BoldGreen,
+                                focusedLabelColor = BoldGreen,
+                                cursorColor = BoldGreen
                             )
-                        },
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            focusedBorderColor = BoldGreen,
-                            focusedLabelColor = BoldGreen,
-                            cursorColor = BoldGreen
                         )
-                    )
-                    OutlinedTextField(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .padding(end = 20.dp, start = 10.dp, top = 20.dp),
-                        value = "",
-                        onValueChange = { },
-                        label = {
-                            Text(text = "Diğer")
-                        },
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowDown,
-                                contentDescription = null,
-                                tint = BoldGreen
+
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, end = 20.dp, top = 20.dp),
+                            value = amount,
+                            onValueChange = { amount = it },
+                            label = {
+                                Text(text = "Gönderilecek Tutar")
+                            },
+                            trailingIcon = {
+                                Text(
+                                    text = "Tüm Bakiye",
+                                    modifier = Modifier.padding(end = 10.dp),
+                                    color = BoldGreen
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = BoldGreen,
+                                focusedLabelColor = BoldGreen,
+                                cursorColor = BoldGreen
                             )
-                        },
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            focusedBorderColor = BoldGreen,
-                            focusedLabelColor = BoldGreen,
-                            cursorColor = BoldGreen
                         )
+
+                        Row {
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                                    .padding(start = 20.dp, end = 10.dp, top = 20.dp),
+                                value = "",
+                                onValueChange = { },
+                                label = {
+                                    Text(text = "Bugün")
+                                },
+                                trailingIcon = {
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowDown,
+                                        contentDescription = null,
+                                        tint = BoldGreen
+                                    )
+                                },
+                                colors = TextFieldDefaults.outlinedTextFieldColors(
+                                    focusedBorderColor = BoldGreen,
+                                    focusedLabelColor = BoldGreen,
+                                    cursorColor = BoldGreen
+                                )
+                            )
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        scope.launch {
+                                            if (sheetState.isVisible) {
+                                                sheetState.hide()
+                                            } else {
+                                                sheetState.show()
+                                            }
+                                        }
+                                    }
+                                    .weight(1f)
+                                    .padding(end = 20.dp, start = 10.dp, top = 20.dp),
+                                value = "",
+                                onValueChange = { },
+                                label = {
+                                    Text(text = "Diğer")
+                                },
+                                trailingIcon = {
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowDown,
+                                        contentDescription = null,
+                                        tint = BoldGreen
+                                    )
+                                },
+                                colors = TextFieldDefaults.outlinedTextFieldColors(
+                                    focusedBorderColor = BoldGreen,
+                                    focusedLabelColor = BoldGreen,
+                                    cursorColor = BoldGreen
+                                ),
+                                readOnly = true
+                            )
+                        }
+
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, top = 20.dp, end = 20.dp),
+                            value = description,
+                            onValueChange = { description = it },
+                            label = {
+                                Text(text = "Açıklama (İsteğe Bağlı)")
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = BoldGreen,
+                                focusedLabelColor = BoldGreen,
+                                cursorColor = BoldGreen
+                            )
+                        )
+
+                        Button(
+                            onClick = { },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(90.dp)
+                                .padding(20.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = BoldGreen,
+                                contentColor = Color.White
+                            )
+                        ) {
+                            Text(text = "DEVAM")
+                        }
+
+                    }
+                },
+                sheetContent = {
+                    AccountList(
+                        accountList = accountList,
+                        onAccountClicked = {
+                            println("Account Model: $it")
+                        }
                     )
                 }
+            )
 
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp, top = 20.dp, end = 20.dp),
-                    value = description,
-                    onValueChange = { description = it },
-                    label = {
-                        Text(text = "Açıklama (İsteğe Bağlı)")
-                    },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = BoldGreen,
-                        focusedLabelColor = BoldGreen,
-                        cursorColor = BoldGreen
-                    )
-                )
-
-                Button(
-                    onClick = { },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(90.dp)
-                        .padding(20.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = BoldGreen,
-                        contentColor = Color.White
-                    )
-                ) {
-                    Text(text = "DEVAM")
-                }
-
-            }
         }
     )
 
 
 }
 
+@ExperimentalMaterialApi
 @Preview(showBackground = true)
 @Composable
 fun UserInformationPagePreview() {
